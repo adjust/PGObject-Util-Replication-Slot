10e04f8decbc1e6ee2910a6b2d3769133241049e
Chris Travers <chris.travers@gmail.com>
2017-07-14 15:41:20 +0200

    Updating license boilerplate to reflect Adjust's ownership

--- lib/PGObject/Util/Replication/Slot.pm
+++ lib/PGObject/Util/Replication/Slot.pm
@@ -219,7 +219,7 @@ L<http://search.cpan.org/dist/PGObject-Util-Replication-Slot/>
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright 2017 Chris Travers.
+Copyright 2017 Adjust.com
 
 This program is distributed under the (Revised) BSD License:
 L<http://www.opensource.org/licenses/BSD-3-Clause>
@@ -235,7 +235,7 @@ notice, this list of conditions and the following disclaimer.
 notice, this list of conditions and the following disclaimer in the
 documentation and/or other materials provided with the distribution.
 
-* Neither the name of Chris Travers's Organization
+* Neither the name of Adjust.com
 nor the names of its contributors may be used to endorse or promote
 products derived from this software without specific prior written
 permission.
