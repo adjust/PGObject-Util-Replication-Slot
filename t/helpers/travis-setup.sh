43921521c61488d5a125a97a46bc2bc483ff566f
Chris Travers <chris.travers@gmail.com>
2017-07-20 11:26:11 +0200

    wal level to replica on helper

--- t/helpers/travis-setup.sh
+++ t/helpers/travis-setup.sh
@@ -2,5 +2,6 @@
 
 psql -c 'ALTER SYSTEM SET max_wal_senders to 5'
 psql -c 'ALTER SYSTEM SET max_replication_slots to 5'
+psql -c 'ALTER SYSTEM SET wal_level to replica'
 sudo service postgresql stop
 sudo service postgresql start 9.6
